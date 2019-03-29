882febac5275b441c224f1385e32769d7ae8a693
KillianG <killian.gardahaut@epitech.eu>
2019-03-29 15:41:35 +0100

    Adding travis to start server too

--- test/tests.js
+++ test/tests.js
@@ -2,24 +2,8 @@ var restify = require('restify-clients');
 var assert = require('assert');
 var chai = require('chai')
 var should = chai.should()
-var client = restify.createJsonClient('http://epicare.fr:8080');
-var enableDestroy = require('server-destroy')
-var server
-var sql = require('../routes/sql')
+var client = restify.createJsonClient('http://localhost:8080');
 
-before(function (done) {
-	server = require('../app')
-	enableDestroy(server)
-	done()
-})
-
-after(function (done) {
-	server.destroy()
-	server.close();
-	setImmediate(function(){server.emit('close')});
-	sql.db.get.end()
-	done()
-})
 
 describe('GET /order/:file', () => {
 	describe('Get an order', () => {
@@ -60,8 +44,7 @@ describe('GET /get_message/:username', () => {
 		it('Should return code 500 for someone who does not exists', (done) => {
 			new Promise ((resolve, reject) => {
 				client.get('/get_message/bonjourjesuispersonne', function(err, req, res, data) {
-					if (res)
-						res.statusCode.should.equal(504)
+					res.statusCode.should.equal(504)
 					resolve()
 				})
 			}).then(() => {
